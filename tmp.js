b33386556117e8fca79a3948c7688126eebfb92b
sherlock <abdullayev.13.dev@gmail.com>
2023-11-02 21:00:38 +0500

    65l .

--- tmp.js
+++ tmp.js
@@ -1,9 +1,29 @@
-var fs = require('fs')
-var conversion = require("phantom-html-to-pdf")();
+const puppeteer = require('puppeteer');
+const fs = require('fs');
 
-conversion({ html: "<h1>Hello World</h1>" }, function(err, pdf) {
-    var output = fs.createWriteStream('custom0.pdf')
-    console.log(pdf.logs);
-    console.log(pdf.numberOfPages);
-    pdf.stream.pipe(output);
-});
+(async () => {
+
+    // Create a browser instance
+    const browser = await puppeteer.launch();
+
+    // Create a new page
+    const page = await browser.newPage();
+
+    //Get HTML content from HTML file
+    const html = fs.readFileSync('index.voucher.html', 'utf-8');
+    await page.setContent(html, {waitUntil: 'domcontentloaded'});
+
+    // To reflect CSS used for screens instead of print
+    await page.emulateMediaType('screen');
+
+    // Downlaod the PDF
+    const pdf = await page.pdf({
+        path: 'result.pdf',
+        printBackground: true,
+        width: '595px',
+        height: '842px',
+    });
+
+    // Close the browser instance
+    await browser.close();
+})();
